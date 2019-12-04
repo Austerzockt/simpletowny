d77a5d256cf193cb33263fe24fab4dee4f2dc2ea
Austerzockt <simon.woegerbauer@gmx.net>
2019-12-04 17:34:05 +0100

    Initialized repos
    Signed-off-by: austerzockt <simon.woegerbauer@gmx.net>

---
+++ plugin_paper/src/main/java/at/austerzockt/simpletowny/classes/Towny.java
@@ -0,0 +1,15 @@
+package at.austerzockt.simpletowny.classes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Towny extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
